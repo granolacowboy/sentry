acf2f98bb6bf262709ec974cfe4b4d74d9f09fad
Buck Evan <buck.evan@sentry.io>
2025-07-03 10:46:35 -0500

    use __name__ for thread_name_prefix

--- src/sentry/taskworker/worker.py
+++ src/sentry/taskworker/worker.py
@@ -180,9 +180,7 @@ class TaskWorker:
 
         def result_thread() -> None:
             logger.debug("taskworker.worker.result_thread_started")
-            iopool = ThreadPoolExecutor(
-                thread_name_prefix="TaskWorker", max_workers=self._concurrency
-            )
+            iopool = ThreadPoolExecutor(thread_name_prefix=__name__, max_workers=self._concurrency)
             with iopool as executor:
                 while not self._shutdown_event.is_set():
                     try:
